1dca9e8de3c66abf36849237108ac2a50f55e320
Reynald Havard <reynald.havard@gmail.com>
2023-03-29 00:16:55 +0100

    add persistent volume to postgresql server

--- start_postgres_server.sh
+++ start_postgres_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --name pgsql-dev -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+docker run \
+    --name pgsql-dev \
+    -e POSTGRES_PASSWORD=password \
+    -p 5432:5432 \
+    -v pgdata:/var/lib/postgresql/data \
+    -d \
+    postgres
