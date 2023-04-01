5e8c829be4cfed36737de0550d3386b3218426d1
Reynald Havard <reynald.havard@gmail.com>
2023-04-01 16:11:46 +0100

    added tests, finished tutorial part 5

--- djangoproject_tutorial/mysite/mysite/settings.py
+++ djangoproject_tutorial/mysite/mysite/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
         "PASSWORD": "password",
         "HOST": "127.0.0.1",
         "PORT": "5432",
+        "TEST": {
+            "NAME": "test_django_db"
+        }
     }
 }
 
